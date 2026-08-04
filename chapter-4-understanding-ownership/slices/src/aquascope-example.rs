0bca96600620e2d078bba12ac046e73f4db97c5f
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2026-08-04 10:43:05 +0100

    studying ownership

--- chapter-4-understanding-ownership/slices/src/aquascope-example.rs
+++ chapter-4-understanding-ownership/slices/src/aquascope-example.rs
@@ -6,8 +6,10 @@ fn main() {
   let n = &v[0]; // indexation goes first n = &(v[0])
   // same as *.index(0)
   // n borrows a reference to the first element of v, which is 1
-  v.push(0);
-  let x = (*n) + 1; // x = 2
-  println!("x = {x}");
+  v.push(0); // It can produce UB: a reallocation of the vector, which invalidates the reference n.
+  v[2] = 5; // v is mutated, but n is still valid because it is a reference to the first element of v
+  let x = (*n) + 1;   //  Move these two lines before the mutation of v  and the error will disappear. 
+  println!("x = {x}"); // The reason is that the borrow checker will see that n is no longer used after the mutation of v, so it will allow the mutation. If we move the lines after the mutation of v, the borrow checker will see that n is still used after the mutation, so it will not allow the mutation.
+
   println!("v = {:?}", v);
 }
